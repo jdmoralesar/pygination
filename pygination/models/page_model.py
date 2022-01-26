774f1b501424c12c1c176f0613bea0e783dfc8d6
Juan Daniel Morales <juan.morales@seon.com>
2022-01-26 16:49:11 -0500

    genericmodel replacement for base model

--- pygination/models/page_model.py
+++ pygination/models/page_model.py
@@ -1,16 +1,16 @@
-from typing import List, Any, Optional,Sequence,TypeVar,Generic
-from pydantic import BaseModel
+from typing import Optional,Sequence,TypeVar,Generic
+from pydantic.generics import GenericModel
 
 T = TypeVar("T")
 
 
-class PageModel(BaseModel,Generic[T]):
+class PageModel(GenericModel, Generic[T]):
     items: Sequence[T]
     has_next: Optional[bool]
     next_page: Optional[int]
     has_previous: Optional[bool]
     total: int
     pages: int
-    
+
     class Config:
         arbitrary_types_allowed = True
