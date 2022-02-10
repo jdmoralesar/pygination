ba40082df47b7ef2b445d7d6ec27de20e7246728
Juan Daniel Morales <juan.morales@seon.com>
2022-02-10 17:50:32 -0500

    0.0.8 version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from os import path
 from setuptools import find_packages, setup
 
 
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 NAME = "pygination"
 AUTHOR = "Enerbit"
 AUTHOR_EMAIL = "jdmoralesar@gmail.com"
